ef15b5b616cbf0046784e796be4fad343757d734
koiNoCirculation <belica.ly42@gmail.com>
2021-02-26 21:40:03 +0800

    daily commit. update miner logic.

--- src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityMiner.java
+++ src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityMiner.java
@@ -7,7 +7,9 @@ import gregtech.GregTechMod;
 import gregtech.api.GTValues;
 import gregtech.api.GregTechAPI;
 import gregtech.api.capability.IEnergyContainer;
+import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
+import gregtech.api.gui.widgets.SlotWidget;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
 import gregtech.api.metatileentity.TieredMetaTileEntity;
@@ -15,19 +17,25 @@ import gregtech.api.metatileentity.WorkableTieredMetaTileEntity;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.render.OrientedOverlayRenderer;
 import gregtech.api.util.GTLog;
+import gregtech.common.blocks.BlockOre;
 import gregtech.common.blocks.NonMetaBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.StringUtils;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.ForgeChunkManager;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
 
 import java.util.List;
 
@@ -53,9 +61,30 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
      */
     private static final int BASE_DURATION = 20 * 4;
 
-    private MinerState state = MinerState.IDLE;
+    private final int DURATION;
 
+    private final int ENERGY_COST;
+
+    //volatile
     private ChunkPos chunkPosController;
+
+    /*
+     * absolute XCoord
+     * volatile
+     * work.x << 4
+     */
+    private int absoluteStartX;
+
+    /*
+     * absolute ZCoord
+     * volatile
+     * work.z << 4
+     */
+    private int absoluteStartZ;
+
+    //these are persistent states
+
+    private MinerState state = MinerState.IDLE;
 
     private ChunkPos work;
 
@@ -73,35 +102,18 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
      */
     private int digZ;
 
-    /*
-     * absolute XCoord
-     * work.x << 4
-     */
-    private int absoluteStartX;
-
-    /*
-     * absolute ZCoord
-     * work.z << 4
-     */
-    private int absoluteStartZ;
-
-    /*
-     * store mining tubes
-     */
-    private ItemStack tubes = new ItemStack(Items.AIR, 0);
-
-    /**
+     /**
      * progress counter
      */
     private int progress = 0;
 
-    /*
-     * store outputs
-     */
-    private ItemStack[] outputs = new ItemStack[9];
+    //end of persistent stats
+
 
     public MetaTileEntityMiner(ResourceLocation metaTileEntityId, int tier) {
         super(metaTileEntityId, tier);
+        ENERGY_COST =  BASE_ENERGY_COST << (2 * (tier - 1));
+        DURATION = BASE_DURATION << (getTier() - 1);
     }
 
     protected int getChunkRadius() {
@@ -142,14 +154,6 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
         unforceChunk(chunkPosController);
     }
 
-    protected int getEnergyCost() {
-        return BASE_ENERGY_COST << (getTier());
-    }
-
-    protected int getDuration() {
-        return BASE_DURATION;
-    }
-
     /**
      * if yccord > 1 and next block is not bedrock
      * @return
@@ -178,6 +182,7 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
             workingLayer--;
         }
         if(canDigDeeper()) {
+            ItemStack tubes = importItems.getStackInSlot(0);
             if (!tubes.isEmpty() && tubes.getItem() == NonMetaBlocks.ITEM_MINING_TUBE) {
                 tubes.shrink(1);
                 world.setBlockState(new BlockPos(current.getX(), workingLayer, current.getZ()), NonMetaBlocks.MINING_TUBE.getDefaultState());
@@ -187,11 +192,27 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
         return success;
     }
 
+    protected boolean tryPutIntoInventory(ItemStack stack) {
+        for (int i = 0; i < exportItems.getSlots(); i++) {
+            ItemStack stackInSlot = exportItems.getStackInSlot(i);
+            if(stackInSlot.isEmpty()) {
+
+            } else if(stackInSlot.getItem() == stack.getItem()) {
+
+            }
+        }
+        return false;
+    }
+
     protected void dig() {
         /**
          * dig it here
          */
         GTLog.logger.info("Digging block<{}, {}>, absolute x,z = <{}, {}> at chunk<{},{}>", digX, digZ, absoluteStartX + digX, absoluteStartZ + digZ, work.x, work.z);
+        Block block = getWorld().getBlockState(new BlockPos(absoluteStartX + digX, workingLayer, absoluteStartZ + digZ)).getBlock();
+        if(block instanceof BlockOre) {
+            ItemStack itemStack = new ItemStack(block, 1);
+        }
         if(digZ >= 16) {
             if(digX < 16) {
                 /**
@@ -226,8 +247,8 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
                     }
                 }
                 work = new ChunkPos(newChunkX, newChunkZ);
-                absoluteStartX = work.x << 4;
-                absoluteStartZ = work.z << 4;
+                absoluteStartX = work.x >> 4;
+                absoluteStartZ = work.z >> 4;
                 forceLoadWorkingChunk();
                 digX = 0;
             }
@@ -244,8 +265,15 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
             switch (state) {
                 case IDLE: break;
                 case RUNNING: {
-                    if(progress < getDuration()) {
-                        progress++;
+                    if(progress < DURATION) {
+                        long cost = energyContainer.changeEnergy(ENERGY_COST);
+                        if(cost < ENERGY_COST) {
+                            //not enough energy
+                            state = MinerState.IDLE;
+                            progress = 0;
+                        } else {
+                            progress++;
+                        }
                     } else {
                         dig();
                         progress = 0;
@@ -260,13 +288,102 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity {
         }
     }
 
+    /**
+     * serialization
+     * @param data
+     * @return
+     */
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        NBTTagCompound nbtTagCompound = super.writeToNBT(data);
+        //store machine state
+        nbtTagCompound.setString("state", state.name());
+        //store working chunk
+        nbtTagCompound.setInteger("workChunkX", work.x);
+        nbtTagCompound.setInteger("workChunkZ", work.z);
+        //store working layer
+        nbtTagCompound.setInteger("workingLayer", workingLayer);
+        //store working coord
+        nbtTagCompound.setInteger("digX", digX);
+        nbtTagCompound.setInteger("digZ", digZ);
+        //store progress
+        nbtTagCompound.setInteger("progress", progress);
+        return nbtTagCompound;
+    }
+
+    /**
+     * deserialization
+     * @param data
+     */
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        super.readFromNBT(data);
+        //restore state
+        state = data.hasKey("state") ? MinerState.IDLE : MinerState.valueOf(data.getString("state"));
+        //restore working chunk
+        work = data.hasKey("workChunkX") && data.hasKey("workChunkZ") ? new ChunkPos(data.getInteger("workChunkX"), data.getInteger("workChunkZ")) :  new ChunkPos(chunkPosController.x - getChunkRadius(), chunkPosController.z - getChunkRadius());
+        absoluteStartX = work.x << 4;
+        absoluteStartZ = work.z << 4;
+        //restore working layer
+        workingLayer = data.hasKey("workingLayer") ?  data.getInteger("workingLayer") : getPos().getY() - 1;
+        //restore working coord
+        digX = data.getInteger("digX");
+        digZ = data.getInteger("digZ");
+        //restore progress
+        progress = data.getInteger("progress");
+
+    }
+
+
+
     @Override
     protected ModularUI createUI(EntityPlayer entityPlayer) {
-        return null;
+        ModularUI.Builder builder = ModularUI.defaultBuilder().
+                image(0, 0, 143, 75, GuiTextures.DISPLAY).
+                label(5, 5, "采矿机", 0xFFFFFF).
+                widget(new SlotWidget(importItems, 0, 20, 20, true, true)
+                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.IN_SLOT_OVERLAY));
+        int outSlotStartX = 60;
+        int outSlotStartY = 60;
+        for(int row = 0; row < 3; row++) {
+            for(int column = 0; column < 6; column++) {
+                builder.widget(new SlotWidget(exportItems, row * 6 + column, outSlotStartX - 6 * 9 + row * 18, outSlotStartY + column * 18, true, false)
+                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY));
+            }
+        }
+        return builder.build(getHolder(), entityPlayer);
     }
 
     @Override
+    protected IItemHandlerModifiable createImportItemHandler() {
+        return new ItemStackHandler(1) {
+            @Override
+            public boolean isItemValid(int slot, ItemStack stack) {
+                return stack.getItem() == NonMetaBlocks.ITEM_MINING_TUBE;
+            }
+        };
+    }
+
+    /**
+     * 18 output slots
+     * row 3
+     * column 6
+     * @return
+     */
+    @Override
+    protected IItemHandlerModifiable createExportItemHandler() {
+        return new ItemStackHandler(18);
+    }
+
+
+    @Override
     public MetaTileEntity createMetaTileEntity(MetaTileEntityHolder holder) {
-        return null;
+        MetaTileEntityMiner metaTileEntityMiner = new MetaTileEntityMiner(metaTileEntityId, getTier());
+        metaTileEntityMiner.chunkPosController = new ChunkPos(holder.getPos().getX() >> 4, holder.getPos().getZ() >> 4);
+        //initialize
+        work = new ChunkPos(chunkPosController.x - getChunkRadius(), chunkPosController.z - getChunkRadius());
+        absoluteStartX = work.x << 4;
+        absoluteStartZ = work.z << 4;
+        return metaTileEntityMiner;
     }
 }
